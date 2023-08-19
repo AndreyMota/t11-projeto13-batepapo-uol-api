76b0738f76b92707e5495b991866b78ada62d902
AndreyMota <andreymotam1@gmail.com>
2023-08-19 20:50:23 -0300

    gitignore added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express, { json } from "express";
 import cors from "cors";
+import { MonogClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 const PORT = 5000;
+
+const mongoClient = new MonogClient("mongodb://localhost:27017/BatepapoUol");
+let db;
+mongoClient.connect()
+.then(() => db = mongoClient.db())
+.catch((err) => console.log(err.message));
 
 app.listen(PORT);
 
