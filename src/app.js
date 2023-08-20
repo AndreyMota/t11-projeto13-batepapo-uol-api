b805c5046589160668249866d360f07ac98f5605
AndreyMota <andreymotam1@gmail.com>
2023-08-19 22:33:54 -0300

    entrada participants cicle done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 
 dotenv.config()
 
@@ -29,12 +30,13 @@ app.get('/participants', async (req, res) => {
         res.status(200).send(lista);
     } catch (error) {
         res.status(500).send(error.message);
-    }
-    
+    } 
 });
 
 app.post('/participants', async (req, res) => {
     /* {name: 'João', lastStatus: 12313123} */
+    
+    const name = req.body.name;
     const finalParticipant = req.body;
     const validation = nameSchema.validate(req.body, { abortEarly: false });
     if (validation.error) {
@@ -42,13 +44,39 @@ app.post('/participants', async (req, res) => {
         return res.status(422).send(errors);
     }
 
+    try {
+        const jatem = await db.collection("participants").findOne({name});
+        if (jatem) {
+            return res.status(409).send("Participante já está cadastrado");
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
     finalParticipant.lastStatus = Date.now();
 
     try {
+        const messi = {
+            from: name,
+            to: 'Todos' ,
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        };
         await db.collection("participants").insertOne(finalParticipant);
+        await db.collection("messages").insertOne(messi);
         res.status(201).send(finalParticipant);
     } catch (error) {
         res.status(500).send(error.message);
     }
     
+});
+
+app.get('/messages', async (req, res) => {
+    try {
+        const lista = await db.collection("messages").find().toArray();
+        res.status(200).send(lista);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 })
