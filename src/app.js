5bf58c43d578766cb23cbcdf87e41cd6a2243ce4
AndreyMota <andreymotam1@gmail.com>
2023-08-19 21:47:23 -0300

    post e get participants

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express, { json } from "express";
 import cors from "cors";
-import { MonogClient } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import joi from "joi";
+
 dotenv.config()
 
 const app = express();
@@ -9,7 +11,11 @@ app.use(cors());
 app.use(json());
 const PORT = 5000;
 
-const mongoClient = new MonogClient("mongodb://localhost:27017/BatepapoUol");
+const nameSchema = joi.object({
+    name: joi.string().required()
+})
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 mongoClient.connect()
 .then(() => db = mongoClient.db())
@@ -17,6 +23,32 @@ mongoClient.connect()
 
 app.listen(PORT);
 
-app.get('/', (req, res) => {
-    res.send('Opa');
+app.get('/participants', async (req, res) => {
+    try {
+        const lista = await db.collection("participants").find().toArray();
+        res.status(200).send(lista);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+    
+});
+
+app.post('/participants', async (req, res) => {
+    /* {name: 'João', lastStatus: 12313123} */
+    const finalParticipant = req.body;
+    const validation = nameSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    finalParticipant.lastStatus = Date.now();
+
+    try {
+        await db.collection("participants").insertOne(finalParticipant);
+        res.status(201).send(finalParticipant);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+    
 })
