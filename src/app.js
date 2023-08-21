e353d204ff9b919764bb38c2b96b0af8b604a159
AndreyMota <andreymotam1@gmail.com>
2023-08-21 08:44:54 -0300

    started route messages

--- src/app.js
+++ src/app.js
@@ -9,11 +9,17 @@ dotenv.config()
 
 const app = express();
 app.use(cors());
-app.use(json());
+app.use(express.json());
 const PORT = 5000;
 
 const nameSchema = joi.object({
     name: joi.string().required()
+});
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
 })
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
@@ -76,6 +82,39 @@ app.get('/messages', async (req, res) => {
     try {
         const lista = await db.collection("messages").find().toArray();
         res.status(200).send(lista);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post('/messages', async (req, res) => {
+    const finalMessage = req.body;
+    const validation = messageSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+    const from = req.headers.user;
+    console.log(from);
+    try {
+        if (from) {
+            const tem = await db.collection("participants").findOne({name: from});
+            if (tem) {
+                finalMessage.from = from;
+            } else {
+                return res.status(422).send("User não cadastrado");
+            }
+        } else {
+            return res.status(422).send("User não presente no headers");
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
+    try {
+        finalMessage.time = dayjs().format('HH:mm:ss');
+        await db.collection("messages").insertOne(finalMessage);
+        res.sendStatus(201);
     } catch (error) {
         res.status(500).send(error.message);
     }
